957dcdeb0c71922eb91a99f957ad136f6d3f6671
Saipykurbanov <kurbanoviliyas@mail.ru>
2024-12-21 15:42:30 +0300

    upd

--- src/orders/urls.js
+++ src/orders/urls.js
@@ -2,5 +2,27 @@ import Elysia, { t } from "elysia";
 import Views from "./views";
 
 const orders = new Elysia({prefix: '/api/orders'})
+    .get('/all', () => Views.getAllOrders())
+    .post('/create', ({body}) => Views.createOrder(body), {
+        schema: {
+            body: {
+                username: t.String(),
+                phone: t.String(),
+                address: t.String(),
+                productid: t.Integer(),
+            }
+        }
+    })
+    .put('/update/:id', ({body, params: {id}, ip, headers}) => Views.updateOrder(ip, headers, id, body), {
+        schema: {
+            body: {
+                username: t.String(),
+                phone: t.String(),
+                address: t.String(),
+                productid: t.Integer(),
+            }
+        }
+    })
+    .delete('/delete/:id', ({params: {id}, ip, headers}) => Views.deleteOrder(id, headers, ip))
 
 export default orders;
--- src/orders/views.js
+++ src/orders/views.js
@@ -17,4 +17,60 @@ Views.getAllOrders = async () => {
     }
 }
 
+Views.createOrder = async (body) => {
+    try {   
+
+        let req = await pool.query(`
+            INSERT INTO orders 
+            (username, phone, address, productid)
+            VALUES ($1, $2, $3, $4)`,
+        [body.username, body.phone, body.address, body.productid])
+
+        return {success: true, data: req.rows[0], message: 'Заказ успешно создан', status: 200}
+
+    } catch(e) {
+        return {success: false, message: e.message, status: 500}
+    }
+}
+
+Views.updateOrder = async (ip, headers, id, body) => {
+    try {
+
+        let access = jwt.checkTokenAdmin(headers, ip)
+        
+        if(!access.success) return access
+
+        let req = await pool.query(`
+            UPDATE orders SET
+            username = $1,
+            phone = $2,
+            address = $3,
+            productid = $4
+            WHERE ordersid = $5
+            RETURNING *`,
+            [body.username, body.phone, body.address, body.productid, id])
+
+        return {success: true, message: 'Заказ обновлен', data: req.rows[0], status: 200}
+
+    } catch(e) {
+        return {success: false, message: e.message, status: 500}
+    }
+} 
+
+Views.deleteOrder = async (id, headers, ip) => {
+    try {
+
+        let access = jwt.checkTokenAdmin(headers, ip)
+        
+        if(!access.success) return access
+
+        let req = await pool.query(`DELETE FROM orders WHERE orders = $1`, [id])
+
+        return {success: true, status: 200, message: 'Заказ удалён', data: req.rows[0]}
+
+    } catch(e) {
+        return {success: false, message: e.message, status: 500}
+    }
+}
+
 export default Views;

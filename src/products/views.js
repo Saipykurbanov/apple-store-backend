f4eea7210e5d6eb26d91407cc71c2b25464738d7
Saipykurbanov <kurbanoviliyas@mail.ru>
2025-01-03 23:42:31 +0300

    upd

--- src/products/views.js
+++ src/products/views.js
@@ -6,16 +6,50 @@ const Views = {}
 
 Views.getAllProducts = async (query) => {
     try {
+
+        /*Сортировка*/
         let order = query.order || 'datetime'
         let sort = query.sort || 'DESC'
 
-        let res = await pool.query(`
-            SELECT * 
+        /*Отправка всех товаров*/
+        let req = await pool.query(`
+            SELECT 
+                products.productid,
+                products.title,
+                products.price,
+                products.main_image,
+                products.datetime,
+                products.memory,
+                products.color,
+                products.colorName,
+                products.article,
+                COALESCE(
+                    json_agg(
+                        jsonb_build_object(
+                            'id', specifications.specificationsid,
+                            'description', specifications.description,
+                            'icon', specifications.icon
+                        )
+                    ) FILTER (WHERE specifications.article IS NOT NULL), 
+                    '[]'
+                ) AS specifications
             FROM products
+            LEFT OUTER JOIN specifications 
+            ON products.article = specifications.article 
+            GROUP BY 
+                products.productid,
+                products.title,
+                products.price,
+                products.main_image,
+                products.datetime,
+                products.memory,
+                products.color,
+                products.colorName,
+                products.article
             ORDER BY ${order} ${sort}
-        `)
+        `);
 
-        return {success: true, status: 200, data: res.rows}
+        return {success: true, status: 200, data: req.rows}
 
     } catch(e) {
         return {success: false, status: 500, message: e.message}
@@ -25,14 +59,15 @@ Views.getAllProducts = async (query) => {
 Views.createProduct = async (body, headers, ip) => {
     try {
         
+        /*Проверка токена*/
         let access = jwt.checkTokenAdmin(headers, ip)
-        
         if(!access.success) return access
         
+        /*Создание изображения товара*/
         let main_image = crypto.randomUUID()
+        await upload.image(main_image, body.main_image, 'products')
 
-        await upload.image(main_image, body.image, 'products')
-
+        /*Создание товара*/
         let res = await pool.query(
             `INSERT INTO products (title, price, main_image, memory, article, color, colorName)
             VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *`, 
@@ -43,56 +78,20 @@ Views.createProduct = async (body, headers, ip) => {
     } catch(e) {
         return {success: false, status: 500, message: e.message}
     }
-
-}
-
-Views.createAllSpecifications = async (body, headers, ip) => {
-    try {
-
-        let access = jwt.checkTokenAdmin(headers, ip)
-        let data = []
-        
-        if(!access.success) return access
-
-
-        if(body.data && body.data?.length > 0) {
-            let create = body.data.map(async (el) => {
-                let imageName = crypto.randomUUID()
-                await upload.image(imageName, el.image, 'products')
-        
-                let res = await pool.query(`
-                    INSERT INTO specifications 
-                    (icon, description, article)
-                    VALUES ($1, $2, $3) RETURNING *`, 
-                    [imageName, el.description, body.article]
-                )
-
-                res = res.rows[0]
-
-                if(res) {
-                    data.push(res)
-                }
-            })
-
-            await Promise.all(create)
-        }
-
-        return {success: true, status: 200, message: 'Характеристика добавлена', data}
-
-    } catch(e) {
-        return {success: false, status: 500, message: e.message}
-    }
 }
 
 Views.createSpecification = async (body, headers, ip) => {
     try {
 
+        /*Проверка токена*/
         let access = jwt.checkTokenAdmin(headers, ip)
         if(!access.success) return access
 
+        /*Создание изображения хар-ки*/
         let imageName = crypto.randomUUID()
-        await upload.image(imageName, body.image, 'products')
+        await upload.image(imageName, body.file, 'products')
 
+        /*Создание хар-ки*/
         let res = await pool.query(`
             INSERT INTO specifications 
             (icon, description, article)
@@ -107,18 +106,21 @@ Views.createSpecification = async (body, headers, ip) => {
     }
 }
 
-Views.updateSpecifocation = async (body, headers, ip, id) => {
+Views.updateSpecification = async (body, headers, ip, id) => {
     try {
 
+        /*Проверка токена*/
         let access = jwt.checkTokenAdmin(headers, ip)
         if(!access.success) return access
 
+        /*Обновление изображения*/
         if(body.image) {
             upload.deleteImage(body.icon, 'products')
             body.icon = crypto.randomUUID()
             await upload.image(body.icon, body.image, 'products')
         }
 
+        /*Обновление хар-ки*/
         let res = await pool.query(`
             UPDATE specifications SET
             icon $1,
@@ -138,31 +140,31 @@ Views.updateSpecifocation = async (body, headers, ip, id) => {
 Views.updateProduct = async (body, headers, ip, id) => {
     try {
 
+        /*Проверка токена*/
         let access = jwt.checkTokenAdmin(headers, ip)
-        
         if(!access.success) return access
 
+        /*Обновление изображения*/
         if(body.mainFile) {
             upload.deleteImage(body.main_image, 'products')
             body.main_image = crypto.randomUUID()
             await upload.image(body.main_image, body.mainFile, 'products')
         }
 
+        /*Обновление товара*/
         let req = await pool.query(`
             UPDATE products SET
             title = $1,
             price = $2,
             main_image = $3,
             memory = $4,
-            article = $5,
-            color = $6,
-            colorName = $7
-            WHERE productid = $8
+            color = $5,
+            colorName = $6
+            WHERE productid = $7
             RETURNING *`, 
-            [body.title, body.price, body.main_image, body.memory, body.article, body.color, body.colorName, id])
+            [body.title, body.price, body.main_image, body.memory, body.color, body.colorName, id])
 
         return {success: true, message: 'Товар успешно обновлён', data: req.rows[0]}
-
 
     } catch(e) {
         return {success: false, status: 500, message: e.message}
@@ -172,27 +174,43 @@ Views.updateProduct = async (body, headers, ip, id) => {
 Views.deleteProduct = async (id, headers, ip) => {
     try {
 
+        /*Проверка токена*/
         let access = jwt.checkTokenAdmin(headers, ip)
-        
         if(!access.success) return access
-
-        let product = await pool.query(`SELECT * FROM products WHERE productid = $1`, [id])
+            
+        /*Удаление товара*/
+        let product = await pool.query(`DELETE FROM products WHERE productid = $1 RETURNING *`, [id])
         product = product.rows[0]
-        
-        if(product) {
-            
-            upload.deleteImage(product.main_image, 'products')
-            
-            product.specifications.map((el) => {
-                upload.deleteImage(el.imageName, 'products')
+        upload.deleteImage(product.main_image, 'products')
+
+        /*Удаление хар-к*/
+        let specifications = await pool.query(`DELETE FROM specifications WHERE article = $1 RETURNING *`, [product.article])
+        if(specifications.rows?.length > 0) {
+            specifications.rows.map((el) => {
+                upload.deleteImage(el.icon, 'products')
             })
-
-            await pool.query(`DELETE FROM products WHERE productid = $1`, [id])
-
-            return {success: true, status: 200, message: 'Товар успешно удалён'}
         }
 
-        return {success: false, status: 400, message: 'Ошибка'}
+        return {success: true, status: 200, message: 'Товар успешно удалён', data: product}
+
+    } catch(e) {
+        return {success: false, status: 500, message: e.message}
+    }
+}
+
+Views.deleteSpecification = async (id, headers, ip) => {
+    try {
+
+        /*Проверка токена*/
+        let access = jwt.checkTokenAdmin(headers, ip)
+        if(!access.success) return access
+
+        /*Удаление хар-ки*/
+        let req = await pool.query(`DELETE FROM specifications WHERE specificationsid = $1 RETURNING *`, [id])
+        let data = req.rows[0]
+        upload.deleteImage(data.icon, 'products')
+
+        return {success: true, message: 'Характеристика удалена', status: 200, data}
 
     } catch(e) {
         return {success: false, status: 500, message: e.message}
